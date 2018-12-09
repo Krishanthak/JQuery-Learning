b6e0181116ef737e04b33e7a3a53b32529b4c8bb
Krishanthak <krishantha9331@gmail.com>
2018-12-09 15:49:39 +0530

    modified 1

--- Pool Game/js/GameWorld.js
+++ Pool Game/js/GameWorld.js
@@ -1,12 +1,21 @@
+const DELTA = 1/100;
+
 function GameWorld() {
-    this.stick = new Stick();
+    this.whiteBall = new Ball(new Vector2(413, 413));    
+    this.stick = new Stick(
+        new Vector2(413, 413), 
+        this.whiteBall.shoot.bind(this.whiteBall)
+    );
 }
 
 GameWorld.prototype.update = function() {
     this.stick.update();
+    this.whiteBall.update(DELTA);
 }
 
 GameWorld.prototype.draw = function() {
     Canvas.drawImage(sprites.background, {x:0, y:0});
+
     this.stick.draw();
+    this.whiteBall.draw();
 }
--- Pool Game/js/Stick.js
+++ Pool Game/js/Stick.js
@@ -1,17 +1,52 @@
-function Stick() {
-    this.position = {x:0, y:400};
+const STICK_ORIGIN = new Vector2(970, 11);
+const STICK_SHOOT_ORIGIN = new Vector2(950, 11);
+
+function Stick(position, onShoot) {
+    // this.position = new Vector2(400, 400);
+    // this.origin = new Vector2(500, 10);
+    this.position = position;
+    this.rotation = 0;
+    this.origin = STICK_ORIGIN.copy();
+    this.power = 0;
+    this.onShoot = onShoot;
 }
 
 Stick.prototype.update = function() {
     // this.position.x++;
     // Testing...
-    this.position = Mouse.position;
+    // this.position = Mouse.position;
 
-    if(Mouse.left.pressed) {
-        console.log("Pressed Left")
+    // if(Mouse.left.pressed) {
+    //     console.log("Pressed Left")
+    // }
+    if(Mouse.left.down) {
+        this.increasePower();
     }
+    else if(this.power > 0) {
+        this.shoot();
+    }
+
+    this.updateRotation();
 }
 
 Stick.prototype.draw = function() {
-    Canvas.drawImage(sprites.stick, this.position);
+    Canvas.drawImage(sprites.stick, this.position, this.origin, this.rotation);
+}
+
+Stick.prototype.updateRotation = function() {
+    let opposite = Mouse.position.y - this.position.y;
+    let adjacent = Mouse.position.x - this.position.x;
+
+    this.rotation = Math.atan2(opposite, adjacent);
+}
+
+Stick.prototype.increasePower = function() {
+    this.power += 100;
+    this.origin.x += 5;
+}
+
+Stick.prototype.shoot = function() {
+    this.onShoot(this.power, this.rotation);
+    this.power = 0;
+    this.origin = STICK_SHOOT_ORIGIN.copy();
 }
